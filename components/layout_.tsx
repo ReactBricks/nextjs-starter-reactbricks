f94954b20ff0e53712adbbcba165087ee1b50e97
Matteo Frana <f2@f2.net>
2022-01-14 17:59:02 +0100

    renamed components to solve filename case problem

--- components/layout_.tsx
+++ components/layout_.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Header from './header'
-import Footer from './footer'
+import Header from './header_'
+import Footer from './footer_'
 
 const Layout: React.FC = ({ children }) => {
   return (
