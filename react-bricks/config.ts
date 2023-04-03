4e83be7bbc740efcd51c2eb7936c6eda2442659f
Paolo Zanotti <paolo.zanotti@reactbricks.com>
2023-04-03 18:06:32 +0200

    ⚙️ Update react bricks config

--- react-bricks/config.ts
+++ react-bricks/config.ts
@@ -8,32 +8,36 @@ import NextLink from "./NextLink"
 const config: types.ReactBricksConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
   apiKey: process.env.API_KEY,
-  pageTypes,
   bricks,
+  pageTypes,
+  customFields: [],
   logo: "/logo.svg",
-  previewPath: "/preview",
-  //==============================
-  // Usually defined dynamically in _app.tsx to manage Dark mode
-  // See the Next with Tailwind CSS starter
-  // contentClassName: 'content',
-  // isDarkColorMode: ...,
-  // toggleColorMode: ...,
+  loginUI: {},
+  contentClassName: "",
+  //defaultTheme: "",
   renderLocalLink: NextLink,
   navigate: (path: string) => Router.push(path),
   loginPath: "/admin",
   editorPath: "/admin/editor",
   playgroundPath: "/admin/playground",
   appSettingsPath: "/admin/app-settings",
+  previewPath: "/preview",
+  getAdminMenu: () => [],
+  isDarkColorMode: false,
+  toggleColorMode: () => {},
   useCssInJs: false,
   appRootElement: "#__next",
   clickToEditSide: types.ClickToEditSide.BottomRight,
-  customFields: [],
-  //responsiveBreakpoints: [{ type: types.DeviceType.Phone, width: 480, label: 'Smartphone'}],
+  //responsiveBreakpoints: [{ type: types.DeviceType.Phone, width: 480, label: "Smartphone" },],
   enableAutoSave: true,
   disableSaveIfInvalidProps: false,
   enablePreview: true,
+  blockIconsPosition: types.BlockIconsPosition.OutsideBlock,
+  enableUnsplash: true,
+  unsplashApiKey: "",
   enablePreviewImage: true,
   enableDefaultEmbedBrick: true,
+  //permissions,  Fine-grained permissions for enterprise plans
 }
 
 export default config
