316c67abf3614052e38f8a4edd97f1d82b08a1b3
Paolo Zanotti <122781046+paolozanotti-rb@users.noreply.github.com>
2023-04-13 18:05:56 +0200

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,13 @@
 import React, { ReactNode, useContext, useEffect, useState } from "react"
 import styles from "../css/layout.module.css"
-import { ReactBricksContext } from "react-bricks"
 
 interface LayoutProps {
   children?: ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [mounted, setMounted] = useState(false)
-  const { isDarkColorMode } = useContext(ReactBricksContext)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) return null
-
   return (
-    <div className={`${isDarkColorMode ? "dark" : "light"} ${styles.container}`}>
+    <div className={`${styles.container}`}>
       <main className={styles.childrenContainer}>{children}</main>
     </div>
   )
