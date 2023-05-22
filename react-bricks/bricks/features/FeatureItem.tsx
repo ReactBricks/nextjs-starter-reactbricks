2298e1860f9ecec110ec3909993f3cc230687f83
Paolo Zanotti <paolo.zanotti@reactbricks.com>
2023-05-22 17:16:53 +0200

    ✏️ Remove prop Link

--- react-bricks/bricks/features/FeatureItem.tsx
+++ react-bricks/bricks/features/FeatureItem.tsx
@@ -1,9 +1,9 @@
-import * as React from "react"
-import { Image, types, Text, Link, Plain } from "react-bricks/frontend"
-import classNames from "classnames"
-import styles from "../../../css/FeatureItem.module.css"
-import { ColsNumber } from "./Features"
-import { icons } from "./defaultImages"
+import * as React from 'react'
+import { Image, types, Text, Link, Plain } from 'react-bricks/frontend'
+import classNames from 'classnames'
+import styles from '../../../css/FeatureItem.module.css'
+import { ColsNumber } from './Features'
+import { icons } from './defaultImages'
 
 export interface FeatureItemProps {
   colsNumber: ColsNumber
@@ -15,11 +15,11 @@ export interface FeatureItemProps {
 
 const getColumnClass = (colsNumber: ColsNumber) => {
   switch (colsNumber) {
-    case "2":
+    case '2':
       return styles.cols2
-    case "3":
+    case '3':
       return styles.cols3
-    case "4":
+    case '4':
       return styles.cols4
   }
 }
@@ -32,7 +32,7 @@ const FeatureItem: types.Brick<FeatureItemProps> = ({
   linkPath,
 }) => {
   const linkTextPlain =
-    typeof linkText === "string" ? linkText : Plain.serialize(linkText)
+    typeof linkText === 'string' ? linkText : Plain.serialize(linkText)
 
   return (
     <div
@@ -43,8 +43,8 @@ const FeatureItem: types.Brick<FeatureItemProps> = ({
     >
       {withIcon && (
         <Image
-          propName='image'
-          alt='feature'
+          propName="image"
+          alt="feature"
           aspectRatio={1}
           imageClassName={styles.imageClassName}
           renderWrapper={({ children }) => {
@@ -55,15 +55,15 @@ const FeatureItem: types.Brick<FeatureItemProps> = ({
 
       <div className={styles.textFeatureItemContainer}>
         <Text
-          propName='title'
-          placeholder='Title...'
+          propName="title"
+          placeholder="Title..."
           renderBlock={(props) => (
             <div className={styles.title}>{props.children}</div>
           )}
         />
         <Text
-          propName='text'
-          placeholder='Title...'
+          propName="text"
+          placeholder="Title..."
           renderBlock={(props) => (
             <div className={styles.textColor}>{props.children}</div>
           )}
@@ -71,7 +71,6 @@ const FeatureItem: types.Brick<FeatureItemProps> = ({
         {withLink && (
           <div className={styles.linkContainer}>
             <Link
-              propName='link'
               href={linkPath}
               className={classNames(
                 styles.linkWrapper,
@@ -81,22 +80,22 @@ const FeatureItem: types.Brick<FeatureItemProps> = ({
               <div>
                 <Text
                   renderBlock={(props) => <p>{props.children}</p>}
-                  placeholder='Link...'
-                  propName='linkText'
+                  placeholder="Link..."
+                  propName="linkText"
                 />
               </div>
               <svg
-                viewBox='0 0 14 14'
-                width='14px'
-                height='14px'
+                viewBox="0 0 14 14"
+                width="14px"
+                height="14px"
                 className={classNames(
                   styles.svgClass,
                   linkTextPlain ? styles.linkTextPlain2 : styles.linkTextPlain3
                 )}
               >
                 <path
-                  fill='currentColor'
-                  d='m11.1 7.35-5.5 5.5a.5.5 0 0 1-.7-.7L10.04 7 4.9 1.85a.5.5 0 1 1 .7-.7l5.5 5.5c.2.2.2.5 0 .7Z'
+                  fill="currentColor"
+                  d="m11.1 7.35-5.5 5.5a.5.5 0 0 1-.7-.7L10.04 7 4.9 1.85a.5.5 0 1 1 .7-.7l5.5 5.5c.2.2.2.5 0 .7Z"
                 ></path>
               </svg>
             </Link>
@@ -107,38 +106,38 @@ const FeatureItem: types.Brick<FeatureItemProps> = ({
   )
 }
 FeatureItem.schema = {
-  name: "feature-item",
-  label: "Feature",
-  category: "main content",
+  name: 'feature-item',
+  label: 'Feature',
+  category: 'main content',
   hideFromAddMenu: true,
-  playgroundLinkLabel: "View source code on Github",
+  playgroundLinkLabel: 'View source code on Github',
   playgroundLinkUrl:
-    "https://github.com/ReactBricks/react-bricks-ui/blob/master/src/website/Features/FeatureItem.tsx",
+    'https://github.com/ReactBricks/react-bricks-ui/blob/master/src/website/Features/FeatureItem.tsx',
 
   getDefaultProps: () => ({
-    title: "The best experience for editors",
-    text: "Your marketing team hates gray forms. Give them the easiest UX.",
+    title: 'The best experience for editors',
+    text: 'Your marketing team hates gray forms. Give them the easiest UX.',
     withIcon: true,
     withLink: false,
     image: icons.PHOTO_STACK,
-    colsNumber: "2",
-    linkText: "",
-    linkPath: "",
+    colsNumber: '2',
+    linkText: '',
+    linkPath: '',
   }),
   sideEditProps: [
     {
-      name: "withIcon",
-      label: "With icon",
+      name: 'withIcon',
+      label: 'With icon',
       type: types.SideEditPropType.Boolean,
     },
     {
-      name: "withLink",
-      label: "With link",
+      name: 'withLink',
+      label: 'With link',
       type: types.SideEditPropType.Boolean,
     },
     {
-      name: "linkPath",
-      label: "Link to",
+      name: 'linkPath',
+      label: 'Link to',
       type: types.SideEditPropType.Text,
       show: ({ withLink }) => !!withLink,
     },
