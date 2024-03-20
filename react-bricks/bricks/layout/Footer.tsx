eb4a8e9c8eef1de58afe5572eb4d90bc9f18c620
paolozanotti-f2 <paolo.zanotti@reactbricks.com>
2024-03-20 13:30:29 +0100

    ✏️ Update renderLink

--- react-bricks/bricks/layout/Footer.tsx
+++ react-bricks/bricks/layout/Footer.tsx
@@ -1,7 +1,7 @@
-import React from "react"
-import { RichText, Image, Repeater, types, Link } from "react-bricks/frontend"
+import React from 'react'
+import { RichText, Image, Repeater, types, Link } from 'react-bricks/frontend'
 
-import styles from "../../../css/Footer.module.css"
+import styles from '../../../css/Footer.module.css'
 
 interface FooterProps {}
 
@@ -11,29 +11,34 @@ const Footer: types.Brick<FooterProps> = ({}) => {
       <section className={styles.section}>
         <div className={styles.container}>
           <div className={styles.elementsInfo}>
-            <Link href='/' className={styles.linkLogo}>
+            <Link href="/" className={styles.linkLogo}>
               <Image
-                propName='logo'
-                alt='Logo'
+                propName="logo"
+                alt="Logo"
                 maxWidth={300}
                 imageClassName={styles.imageLogo}
               />
             </Link>
             <RichText
-              propName='copyright'
-              placeholder='Copyright notice'
+              propName="copyright"
+              placeholder="Copyright notice"
               renderBlock={({ children }) => (
                 <p className={styles.paragraphRichText}>{children}</p>
               )}
               allowedFeatures={[types.RichTextFeatures.Link]}
-              renderLink={({ children, href }) => (
-                <Link href={href} className={styles.renderLink}>
+              renderLink={({ children, href, target, rel }) => (
+                <Link
+                  href={href}
+                  target={target}
+                  rel={rel}
+                  className={styles.renderLink}
+                >
                   {children}
                 </Link>
               )}
             />
           </div>
-          <Repeater propName='columns' />
+          <Repeater propName="columns" />
         </div>
       </section>
     </footer>
@@ -41,125 +46,125 @@ const Footer: types.Brick<FooterProps> = ({}) => {
 }
 
 Footer.schema = {
-  name: "footer",
-  label: "Footer",
-  category: "layout",
-  tags: ["footer"],
+  name: 'footer',
+  label: 'Footer',
+  category: 'layout',
+  tags: ['footer'],
   repeaterItems: [
     {
-      name: "columns",
-      itemType: "footer-column",
+      name: 'columns',
+      itemType: 'footer-column',
       max: 4,
     },
   ],
   // Defaults when a new brick is added
   getDefaultProps: () => ({
     logo: {
-      src: "https://images.reactbricks.com/original/7fd7ef1a-928f-45d6-b7a7-ff34bf91c15e.svg",
+      src: 'https://images.reactbricks.com/original/7fd7ef1a-928f-45d6-b7a7-ff34bf91c15e.svg',
       placeholderSrc:
-        "https://images.reactbricks.com/original/7fd7ef1a-928f-45d6-b7a7-ff34bf91c15e.svg",
-      srcSet: "",
-      alt: "React Bricks",
-      seoName: "react-bricks",
+        'https://images.reactbricks.com/original/7fd7ef1a-928f-45d6-b7a7-ff34bf91c15e.svg',
+      srcSet: '',
+      alt: 'React Bricks',
+      seoName: 'react-bricks',
       width: 1700.787,
       height: 377.953,
     },
     copyright: [
       {
-        type: "paragraph",
+        type: 'paragraph',
         children: [
           {
-            text: "© React Bricks, Inc.",
+            text: '© React Bricks, Inc.',
           },
         ],
       },
       {
-        type: "paragraph",
+        type: 'paragraph',
         children: [
           {
-            text: "Proudly made in Italy",
+            text: 'Proudly made in Italy',
           },
         ],
       },
     ],
     columns: [
       {
-        title: "Company",
+        title: 'Company',
         links: [
           {
-            linkText: "About us",
-            linkPath: "/",
+            linkText: 'About us',
+            linkPath: '/',
           },
           {
-            linkText: "Why React Bricks?",
-            linkPath: "/",
+            linkText: 'Why React Bricks?',
+            linkPath: '/',
           },
           {
-            linkText: "Terms of service",
-            linkPath: "/",
+            linkText: 'Terms of service',
+            linkPath: '/',
           },
           {
-            linkText: "Privacy",
-            linkPath: "/",
+            linkText: 'Privacy',
+            linkPath: '/',
           },
         ],
       },
       {
-        title: "Features",
+        title: 'Features',
         links: [
           {
-            linkText: "Visual editing",
-            linkPath: "/",
+            linkText: 'Visual editing',
+            linkPath: '/',
           },
           {
-            linkText: "React components",
-            linkPath: "/",
+            linkText: 'React components',
+            linkPath: '/',
           },
           {
-            linkText: "Enterprise-ready",
-            linkPath: "/",
+            linkText: 'Enterprise-ready',
+            linkPath: '/',
           },
           {
-            linkText: "Roadmap",
-            linkPath: "/",
+            linkText: 'Roadmap',
+            linkPath: '/',
           },
         ],
       },
       {
-        title: "Use cases",
+        title: 'Use cases',
         links: [
           {
-            linkText: "Content editors",
-            linkPath: "/",
+            linkText: 'Content editors',
+            linkPath: '/',
           },
           {
-            linkText: "Developers",
-            linkPath: "/",
+            linkText: 'Developers',
+            linkPath: '/',
           },
           {
-            linkText: "Enterprises",
-            linkPath: "/",
+            linkText: 'Enterprises',
+            linkPath: '/',
           },
         ],
       },
       {
-        title: "Learn",
+        title: 'Learn',
         links: [
           {
-            linkText: "Tutorial",
-            linkPath: "/",
+            linkText: 'Tutorial',
+            linkPath: '/',
           },
           {
-            linkText: "Documentation",
-            linkPath: "/",
+            linkText: 'Documentation',
+            linkPath: '/',
           },
           {
-            linkText: "Videos",
-            linkPath: "/",
+            linkText: 'Videos',
+            linkPath: '/',
           },
           {
-            linkText: "Blog",
-            linkPath: "/",
+            linkText: 'Blog',
+            linkPath: '/',
           },
         ],
       },
